e98dac05cf61e66aef6f6c2be9aa513568161bc1
Eric Lemieux <eric@lemieuxdev.com>
2022-07-21 17:28:50 -0400

    Split out parsing of command

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,29 @@ use regex::Regex;
 use std::ops::Range;
 
 pub fn roll(command: &str) -> Result<u32, String> {
-    let dice_roll_regex = Regex::new(r"(?P<number>\d+)(?:d(?P<sides>\d+))?").unwrap();
+    let res = match parse_command(command) {
+        Ok(res) => res,
+        Err(err) => {
+            return Result::Err(err);
+        }
+    };
+
+    let mut sum = 0;
+
+    for _ in 0..res.number {
+        sum += roll_dice(res.sides)
+    }
+
+    Result::Ok(sum)
+}
+
+struct DiceCommand {
+    number: i32,
+    sides: u32,
+}
+
+fn parse_command(command: &str) -> Result<DiceCommand, String> {
+    let dice_roll_regex = Regex::new(r"^(?P<number>\d+) ?(?:d(?P<sides>\d+))$?").unwrap();
 
     let cap = match dice_roll_regex.captures(command) {
         Some(c) => c,
@@ -20,13 +42,10 @@ pub fn roll(command: &str) -> Result<u32, String> {
         .map_or(1, |m| m.as_str().parse().unwrap());
     let dice_sides = cap.name("sides").map_or(6, |m| m.as_str().parse().unwrap());
 
-    let mut sum = 0;
-
-    for _ in 0..number_of_dice {
-        sum += roll_dice(dice_sides)
-    }
-
-    Result::Ok(sum)
+    Result::Ok(DiceCommand {
+        number: number_of_dice,
+        sides: dice_sides,
+    })
 }
 
 /// Roll a dice with a given number of sides.
@@ -40,7 +59,7 @@ fn roll_dice(sides: u32) -> u32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::roll_dice;
+    use crate::*;
 
     #[test]
     fn rolls_within_range() {
@@ -50,5 +69,25 @@ mod tests {
             assert_eq!(false, res > 20);
             assert_eq!(false, res < 1);
         }
+    }
+
+    #[test]
+    fn parse_invalid_roll() {
+        let res = parse_command("1e20");
+        assert_eq!(true, res.is_err());
+    }
+
+    #[test]
+    fn parse_valid_roll() {
+        let res = parse_command("1d20").unwrap();
+        assert_eq!(1, res.number);
+        assert_eq!(20, res.sides);
+    }
+
+    #[test]
+    fn parse_valid_roll_with_space() {
+        let res = parse_command("1 d20").unwrap();
+        assert_eq!(1, res.number);
+        assert_eq!(20, res.sides);
     }
 }
